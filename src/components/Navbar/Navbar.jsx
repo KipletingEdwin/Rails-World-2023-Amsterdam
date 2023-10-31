e940b0c7a08cbc86f75bd994c07fc5eded050f60
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-10-31 18:57:32 +0300

    add background color

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-import railsImage from '../../assets/rails-world-logo.png'
+import React from "react";
+import styles from "./Navbar.module.css";
+import railsImage from "../../assets/rails-world-logo.png";
 
 function Navbar() {
   return (
     <section className={styles.container}>
-        <div className={styles.introduction}>
-            <p> Rails World 2024 will take place in Toronto. Stay tuned for more details! </p>
-        </div>
+      <div className={styles.introduction}>
+        <p>
+          Rails World 2024 will take place in Toronto. Stay tuned for more
+          details!
+        </p>
+      </div>
+      <div className={styles.linksContainer}>
         <div className={styles.navbarLinks}>
-            <img src={railsImage} alt='raisl-world-2023-image'/>
-            <a href='/speakers'>Speakers</a>
-            <a href='/agenda'>Agenda</a>
-            <a href='/speakers'>FAQs</a>
-            <button>View all videos</button>
+          <img src={railsImage} alt="raisl-world-2023-image" />
         </div>
-        
+        <div className={styles.myLinks}>
+            <ul>
+                <li>
+                    <a href="/speakers">Speakers</a>
+                </li>
+                <li>
+                    <a href="/agenda">Agenda</a>
+                </li>
+                <li>
+                    <a href="/faqs">FAQs</a>
+                </li>
+                <li><button>View all videos</button></li>
+            </ul>
+         
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
