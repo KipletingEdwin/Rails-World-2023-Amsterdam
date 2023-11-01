367a63b43e982450c91e5f9738bb39416ca157a2
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-11-02 01:24:51 +0300

    style the code of conduct

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import styles from "./Speakers.module.css";
 
 function Speakers() {
--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import styles from './Sponsors.module.css'
 
 function Sponsors() {
